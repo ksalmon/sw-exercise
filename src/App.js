eb6e68000b4e59f461ad7567fb26403618be3aa1
ksalmon <kyleraysalmon@gmail.com>
2020-04-02 23:02:41 -0400

    Transfer from typescript to ReactApp JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import AppStore from './state/store'
+import AppNav from './components/AppNav'
+import CharacterDashboard from './scenes/CharacterDashboard'
+import CharacterShow from './scenes/CharacterShow'
+
+const App = () => {
+  return ( 
+    <AppStore>
+      <Router>
+        <AppNav />
+        <Switch>
+          <Route path="/people/:id" component={CharacterShow} />
+          <Route path="/" component={CharacterDashboard} />
+        </Switch>
+      </Router>
+    </AppStore>
   );
 }
 
